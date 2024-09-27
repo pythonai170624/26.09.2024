a8059b013509d206dd2a86f4552c7b7604eb70cb
fullstack290623 <fullstack290623@gmail.com>
2024-09-27 10:00:40 +0300

    upload

--- triggers_views.sql
+++ triggers_views.sql
@@ -96,27 +96,3 @@ update products_orders
 set amount = 2
 where order_id = 1 and product_id = 3;
 
---VIEW --------------------------------------------------------
-
-CREATE VIEW order_details_view AS
-SELECT
-    o.order_id,
-    o.date_time,
-    o.address,
-    o.customer_name,
-    o.customer_ph,
-    p.name AS product_name,
-    po.amount,
-    p.price,
-    (po.amount * p.price) AS total_price_per_product,
-    o.total_price
-FROM
-    orders o
-JOIN
-    products_orders po ON o.order_id = po.order_id
-JOIN
-    products p ON po.product_id = p.product_id;
-
-select * from order_details_view where order_id = 1;
-
-
